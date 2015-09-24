622f87a82a9d4809d7e1451b13e51fdf91555739
Hou Changjun <changjun.hou@sap.com>
2015-09-24 23:11:11 +0800

    add return value for UserController

--- src/main/java/com/sap/hackthon/controller/UserController.java
+++ src/main/java/com/sap/hackthon/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sap.hackthon.utils.GlobalConstants;
 
@@ -17,14 +18,15 @@ import com.sap.hackthon.utils.GlobalConstants;
 public class UserController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public void login(@RequestParam String user, @RequestParam String password, HttpServletRequest request) {
+    public @ResponseBody boolean login(@RequestParam String user, @RequestParam String password, HttpServletRequest request) {
         List<String> validUsers = new ArrayList<String>();
         validUsers.add("A");
         validUsers.add("B");
         if (!validUsers.contains(user)) {
-            return;
+            return false;
         }
         HttpSession session = request.getSession();
         session.setAttribute(GlobalConstants.TENANT, user + "_Tenant");
+        return true;
     }
 }
