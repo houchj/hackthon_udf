17d1b2aa4938980160fca5edc47de33a71942f68
Wang.Jun <taurusjun@126.com>
2015-09-24 16:20:20 +0800

    new

--- src/main/webapp/resources/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	bindLanguageSelectHandler();
-	
-	$('#backPortal2sp').click(function() {
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST","call??solutionportal=", false);
-		xhr.send();		
-	});		
-});
