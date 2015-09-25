a51e4a9f084c174a8a090de20988aac727fd5d3e
Hou Changjun <changjun.hou@sap.com>
2015-09-25 09:57:14 +0800

    login function works now.

--- src/main/java/com/sap/hackthon/controller/UserController.java
+++ src/main/java/com/sap/hackthon/controller/UserController.java
@@ -1,28 +1,34 @@
 package com.sap.hackthon.controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sap.hackthon.dto.User;
 import com.sap.hackthon.utils.GlobalConstants;
 
 @Controller
 public class UserController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public @ResponseBody boolean login(@RequestParam String user, @RequestParam String password, HttpServletRequest request) {
+    @RequestMapping(method = RequestMethod.POST, value = "login")
+    public @ResponseBody boolean login(@RequestBody User user, HttpServletRequest request,
+    		HttpServletResponse response) throws ServletException, IOException {
         List<String> validUsers = new ArrayList<String>();
         validUsers.add("A");
         validUsers.add("B");
-        if (!validUsers.contains(user)) {
+        if (!validUsers.contains(user.getUsername())) {
             return false;
         }
         HttpSession session = request.getSession();
