28a91beabdeb9c885322bb31043acf73d4e502f3
Wang.Jun <taurusjun@126.com>
2015-09-25 08:55:18 +0800

    add list entity

--- src/main/java/com/sap/hackthon/services/EntityServiceImpl.java
+++ src/main/java/com/sap/hackthon/services/EntityServiceImpl.java
@@ -1,8 +1,11 @@
 package com.sap.hackthon.services;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -12,7 +15,6 @@ import org.springframework.stereotype.Service;
 
 import com.sap.hackthon.entity.DynamicEntity;
 import com.sap.hackthon.entity.PropertyMeta;
-import com.sap.hackthon.enumeration.UDFTypeEnum;
 import com.sap.hackthon.repository.PropertyMetaRepository;
 
 @Service
@@ -64,8 +66,29 @@ public class EntityServiceImpl implements EntityService {
 
     @Override
     public List<DynamicEntity> list(String objectType, String tanentId) {
+    	List<PropertyMeta> metas=this.getMetas(objectType, tanentId);
+    	Map<String,String> internalNameDisplayNameMap=this.getInternalNameDisplayNameMap(metas);
+    	Map<String,String> conditionMap=new HashMap<String, String>();
+    	conditionMap.put("TENANT_ID", tanentId);
     	
-        return null;
+    	List<DynamicEntity> entities=new ArrayList<DynamicEntity>();
+    	
+    	String selectStr=this.buildSelectClause(objectType, internalNameDisplayNameMap, conditionMap);
+    	System.out.println(selectStr);
+    	List reslt=entityManager.createNativeQuery(selectStr).getResultList();
+    	String[] keys = internalNameDisplayNameMap.keySet().toArray(new String[]{});
+    	for (Object rslt : reslt) {
+    		Object[] objs=(Object[])rslt; 
+    		DynamicEntity entity=new DynamicEntity(objectType);
+    		entities.add(entity);
+    		Map<String, Object> propertities=new HashMap<String, Object>();
+    		for (int i = 0; i < objs.length; i++) {
+    			propertities.put(keys[i], objs[i]);
+			}
+    		entity.setPropertities(propertities);
+		}
+    	
+        return entities;
     }
 
     @Override
@@ -73,7 +96,25 @@ public class EntityServiceImpl implements EntityService {
         return null;
     }
 
-	private List<PropertyMeta> getMetas(
+    private Map<String, String> getInternalNameDisplayNameMap(List<PropertyMeta> metas){
+    	Map<String,String> internalNameDisplayNameMap=new HashMap<String, String>();
+    	for (PropertyMeta propertyMeta : metas) {
+			internalNameDisplayNameMap.put(propertyMeta.getInternalName(), propertyMeta.getDisplayName());
+		}
+    	
+    	return internalNameDisplayNameMap;
+    }
+    
+    private Map<String, Object> getInternalNameValueMap(Map<String, Object> propertities, List<PropertyMeta> metas){
+    	Map<String,Object> internalNameValueMap=new HashMap<String, Object>();
+    	for (PropertyMeta propertyMeta : metas) {
+			internalNameValueMap.put(propertyMeta.getInternalName(), propertities.get(propertyMeta.getDisplayName()));
+		}
+    	
+    	return internalNameValueMap;
+    }
+
+    private List<PropertyMeta> getMetas(
 			DynamicEntity dynamicEntity, String tanentId) {
 		//TODO display name could not duplicated
 		Map<String, String> map = new HashMap<String, String>();
@@ -87,12 +128,28 @@ public class EntityServiceImpl implements EntityService {
 				.setParameter("displayName",
 						dynamicEntity.getPropertities().keySet())
 				.getResultList();
-
+		
 		return metas;
-
 	}
 	
-	private String buildInsertClause(DynamicEntity dynamicEntity, String tanentId, List<PropertyMeta> metas){
+	
+    private List<PropertyMeta> getMetas(
+			String objectType, String tanentId) {
+		//TODO display name could not duplicated
+		Map<String, String> map = new HashMap<String, String>();
+		String jpql = "select pm from "
+				+ PropertyMeta.class.getName()
+				+ " pm where pm.tenantId=:tenantId and pm.objectName=:objectName";
+		List<PropertyMeta> metas = entityManager
+				.createQuery(jpql, PropertyMeta.class)
+				.setParameter("tenantId", tanentId)
+				.setParameter("objectName", objectType)
+				.getResultList();
+		
+		return metas;
+	}
+
+    private String buildInsertClause(DynamicEntity dynamicEntity, String tanentId, List<PropertyMeta> metas){
 		String tbName=dynamicEntity.getObjectType();
 		String orderIdColumnName="ORDER_ID";
 
@@ -130,6 +187,42 @@ public class EntityServiceImpl implements EntityService {
 		return builder.toString();
 	}
 
+	private String buildSelectClause(String tbName, Map<String,String> internalNameDisplayNameMap, Map<String,String> conditionMap){
+		StringBuilder whereBuilder=new StringBuilder();
+		for (Entry<String, String> entry : conditionMap.entrySet()) {
+			if(whereBuilder.length()!=0){
+				whereBuilder.append(" AND ");
+			}
+			whereBuilder.append(entry.getKey());
+			whereBuilder.append("=");
+			whereBuilder.append("'");
+			whereBuilder.append(entry.getValue());
+			whereBuilder.append("'");
+		}
+
+		StringBuilder selectBuilder=new StringBuilder();
+		for (Entry<String, String> entry : internalNameDisplayNameMap.entrySet()) {
+			if(selectBuilder.length()!=0){
+				selectBuilder.append(", ");
+			}
+			selectBuilder.append(entry.getKey());
+			selectBuilder.append(" ");
+			selectBuilder.append(entry.getValue());
+		}
+		
+		StringBuilder builder=new StringBuilder();
+		builder.append("SELECT ");
+		builder.append(selectBuilder.toString());
+		builder.append(" FROM ");
+		builder.append(tbName);
+		builder.append(" ");
+		builder.append("WHERE ");
+		builder.append(whereBuilder.toString());
+		builder.append(" ");
+		
+		return builder.toString();
+	}
+	
 	private String buildSelectClause(DynamicEntity dynamicEntity, String tanentId, List<PropertyMeta> metas){
 		String tbName=dynamicEntity.getObjectType();
 		String orderIdColumnName="ORDER_ID";
