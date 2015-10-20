fa5e0a70de4a7ee529f2863a0dac84a353b30fc6
Junwei Ma <junwei.ma@sap.com>
2015-10-20 19:37:41 +0800

    refine structure

--- src/main/java/com/sap/hackthon/services/PropertyCountingService.java
+++
@@ -1,6 +0,0 @@
-package com.sap.hackthon.services;
-
-public interface PropertyCountingService {
-	public void addReferenceCounting(String objectType, String fieldName, Long counting);
-	public void minusReferenceCounting(String objectType, String fieldName, Long counting);
-}
